a6bc0f3a9a1e72d6eab2c1a765916adec18cf9dc
kriepnieks <kriepnieks@whitecryption.com>
2020-01-29 19:35:56 +0200

    d7-2

--- d7/ezcode.cpp
+++ d7/ezcode.cpp
@@ -7,12 +7,18 @@
 
 using namespace std;
 
-void intcode(vector<int> &rom);
 int intcode(vector<int> rom, vector<int> &inputs);
+int intcode(struct image &img, int input);
 
 vector<int> rom;
 
 vector<vector<int>> permutations;
+
+struct image {
+	vector<int> rom;
+	int ip;
+	int mode;
+};
 
 enum ops {
 	add=1, mult=2, inp=3, out=4, quit=99, jit=5, jif=6, lt=7, eq=8
@@ -75,308 +81,184 @@ int main(int argc, char* argv[]) {
 		if (c == EOF) break;
 		val = 10*val+c-'0';
 	}
-	vector<int> arr = {0,1,2,3,4};
+	vector<int> arr = {5,6,7,8,9};
 	HeapPermuts(arr, arr.size());
 	int max = 0;
+	struct image img1, img2,img3,img4,img5;
+	img1 = img2 = img3 = img4 = img5 = {rom,0};
 
 	for (vector<int> p : permutations) {
-		int out = 0;
-		vector<int> inp(2);
+		int num = 0;
+		struct image img1, img2,img3,img4,img5;
+		img1 = img2 = img3 = img4 = img5 = {rom,0};
+		img1.mode = p[0];
+		img2.mode = p[1];
+		img3.mode = p[2];
+		img4.mode = p[3];
+		img5.mode = p[4];
+		while (num != -99){
+			num = intcode(img1, num);
+			if (num > max) max = num;
+			num = intcode(img2, num);
+			if (num > max) max = num;
+			num = intcode(img3, num);
+			if (num > max) max = num;
+			num = intcode(img4, num);
+			if (num > max) max = num;
+			num = intcode(img5, num);
+			if (num > max) max = num;
+		}
 
-		for (int i = 0; i < p.size(); i++) {
-			inp = {p[i], out};
-			out = intcode(rom, inp);
-		}
-		if (out > max) {
-			max = out;
-		}
 	}
 	cout << max << endl;
 	
 }
 
-int intcode(vector<int> rom, vector<int> &inputs) {
+int intcode(struct image &img, int input) {
 	int param1 = 0;
 	int param2 = 0;
 	int loc = 0;
 	int ins;
-	int inpIT = 0;
 	int output = -99;
-	int ip = 0;
 
-	while (ip < rom.size()) {
-		ins = rom[ip] % 100;
+	while (img.ip < img.rom.size()) {
+		ins = img.rom[img.ip] % 100;
 		switch(ins){
 			case add:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
+				if ( img.rom[img.ip] % 1000 > 100) {
+					param1 = img.rom[img.ip+1];
 				} else {
-					param1 = rom[rom[ip+1]];
+					param1 = img.rom[img.rom[img.ip+1]];
 				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
+				if (rom[img.ip]%10000 > 1000) {
+					param2 = img.rom[img.ip+2];
 				} else {
-					param2 = rom[rom[ip+2]];
+					param2 = img.rom[img.rom[img.ip+2]];
 				}
-				loc = rom[ip+3];
-				rom[loc] = param1 + param2;
-				ip = ip + 4;
+				loc = img.rom[img.ip+3];
+				img.rom[loc] = param1 + param2;
+				img.ip = img.ip + 4;
 				break;
 			case mult:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
+				if ( img.rom[img.ip] % 1000 > 100) {
+					param1 = img.rom[img.ip+1];
 				} else {
-					param1 = rom[rom[ip+1]];
+					param1 = img.rom[img.rom[img.ip+1]];
 				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
+				if (rom[img.ip]%10000 > 1000) {
+					param2 = img.rom[img.ip+2];
 				} else {
-					param2 = rom[rom[ip+2]];
+					param2 = img.rom[img.rom[img.ip+2]];
 				}
-				loc = rom[ip+3];
-				rom[loc] = param1 * param2;
-				ip = ip + 4;
+				loc = img.rom[img.ip+3];
+				img.rom[loc] = param1 * param2;
+				img.ip = img.ip + 4;
 				break;
 			case inp:
 				int tmp;
 				//cin.tie(0);
 				//cout << "input number: ";
 				//cin >> tmp;
-				param1 = rom[ip+1];
-				rom[param1] = inputs[inpIT++];
-				ip = ip + 2;
+				param1 = img.rom[img.ip+1];
+				if (img.mode) {
+					img.rom[param1] = img.mode;
+					img.mode = 0;
+				} else {
+					img.rom[param1] = input;
+				}
+				img.ip = img.ip + 2;
 				break;
 			case out:
-				if (rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
+				if (img.rom[img.ip] % 1000 > 100) {
+					param1 = img.rom[img.ip+1];
 				} else {
-					param1 = rom[rom[ip+1]];
+					param1 = img.rom[img.rom[img.ip+1]];
 				}
-				//cout << "OUTPUT: " << param1 << endl;
+				cout << "OUTPUT: " << param1 << endl;
 				output = param1;
-				ip = ip+2;
-				break;
+				img.ip = img.ip+2;
+				return output;
 			case jit:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
+				if ( img.rom[img.ip] % 1000 > 100) {
+					param1 = img.rom[img.ip+1];
 				} else {
-					param1 = rom[rom[ip+1]];
+					param1 = img.rom[img.rom[img.ip+1]];
 				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
+				if (img.rom[img.ip]%10000 > 1000) {
+					param2 = img.rom[img.ip+2];
 				} else {
-					param2 = rom[rom[ip+2]];
+					param2 = img.rom[img.rom[img.ip+2]];
 				}
 				if (param1 != 0) {
-					ip = param2;
+					img.ip = param2;
 				} else {
-					ip = ip + 3;
+					img.ip = img.ip + 3;
 				}
 				break;
 			case jif:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
+				if ( img.rom[img.ip] % 1000 > 100) {
+					param1 = img.rom[img.ip+1];
 				} else {
-					param1 = rom[rom[ip+1]];
+					param1 = img.rom[img.rom[img.ip+1]];
 				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
+				if (img.rom[img.ip]%10000 > 1000) {
+					param2 = img.rom[img.ip+2];
 				} else {
-					param2 = rom[rom[ip+2]];
+					param2 = img.rom[img.rom[img.ip+2]];
 				}
 				if (param1 == 0) {
-					ip = param2;
+					img.ip = param2;
 				} else {
-					ip = ip + 3;
+					img.ip = img.ip + 3;
 				}
 				break;
 			case lt:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
+				if ( img.rom[img.ip] % 1000 > 100) {
+					param1 = img.rom[img.ip+1];
 				} else {
-					param1 = rom[rom[ip+1]];
+					param1 = img.rom[img.rom[img.ip+1]];
 				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
+				if (img.rom[img.ip]%10000 > 1000) {
+					param2 = img.rom[img.ip+2];
 				} else {
-					param2 = rom[rom[ip+2]];
+					param2 = img.rom[img.rom[img.ip+2]];
 				}
-				loc = rom[ip+3];
+				loc = img.rom[img.ip+3];
 
 				if (param1 < param2) {
-					rom[loc] = 1;
+					img.rom[loc] = 1;
 				} else {
-					rom[loc] = 0;
+					img.rom[loc] = 0;
 				}
-				ip = ip + 4;
+				img.ip = img.ip + 4;
 				break;
 			case eq:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
+				if ( img.rom[img.ip] % 1000 > 100) {
+					param1 = img.rom[img.ip+1];
 				} else {
-					param1 = rom[rom[ip+1]];
+					param1 = img.rom[img.rom[img.ip+1]];
 				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
+				if (img.rom[img.ip]%10000 > 1000) {
+					param2 = img.rom[img.ip+2];
 				} else {
-					param2 = rom[rom[ip+2]];
+					param2 = img.rom[img.rom[img.ip+2]];
 				}
-				loc = rom[ip+3];
+				loc = img.rom[img.ip+3];
 				if (param1 == param2) {
-					rom[loc] = 1;
+					img.rom[loc] = 1;
 				} else {
-					rom[loc] = 0;
+					img.rom[loc] = 0;
 				}
-				ip = ip + 4;
+				img.ip = img.ip + 4;
 				break;
 			case quit:
 				//cout << "exit op" << endl;
-				return output;
+				return -99;
 			default:
-				//cout << "bad op: " << rom[ip] << endl;
+				//cout << "bad op: " << rom[img.ip] << endl;
 				return -99;
 		}
 	}
 	return -99;
 }
 
-void intcode(vector<int> &rom) {
-	int param1 = 0;
-	int param2 = 0;
-	int loc = 0;
-	int ins;
-	int ip = 0;
-
-	while (ip < rom.size()) {
-		ins = rom[ip] % 100;
-		switch(ins){
-			case add:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
-				} else {
-					param1 = rom[rom[ip+1]];
-				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
-				} else {
-					param2 = rom[rom[ip+2]];
-				}
-				loc = rom[ip+3];
-				rom[loc] = param1 + param2;
-				ip = ip + 4;
-				break;
-			case mult:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
-				} else {
-					param1 = rom[rom[ip+1]];
-				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
-				} else {
-					param2 = rom[rom[ip+2]];
-				}
-				loc = rom[ip+3];
-				rom[loc] = param1 * param2;
-				ip = ip + 4;
-				break;
-			case inp:
-				int tmp;
-				cin.tie(0);
-				cout << "input number: ";
-				cin >> tmp;
-				param1 = rom[ip+1];
-				rom[param1] = tmp;
-				ip = ip + 2;
-				break;
-			case out:
-				if (rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
-				} else {
-					param1 = rom[rom[ip+1]];
-				}
-				cout << "OUTPUT: " << param1 << endl;
-				ip = ip+2;
-				break;
-			case jit:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
-				} else {
-					param1 = rom[rom[ip+1]];
-				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
-				} else {
-					param2 = rom[rom[ip+2]];
-				}
-				if (param1 != 0) {
-					ip = param2;
-				} else {
-					ip = ip + 3;
-				}
-				break;
-			case jif:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
-				} else {
-					param1 = rom[rom[ip+1]];
-				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
-				} else {
-					param2 = rom[rom[ip+2]];
-				}
-				if (param1 == 0) {
-					ip = param2;
-				} else {
-					ip = ip + 3;
-				}
-				break;
-			case lt:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
-				} else {
-					param1 = rom[rom[ip+1]];
-				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
-				} else {
-					param2 = rom[rom[ip+2]];
-				}
-				loc = rom[ip+3];
-
-				if (param1 < param2) {
-					rom[loc] = 1;
-				} else {
-					rom[loc] = 0;
-				}
-				ip = ip + 4;
-				break;
-			case eq:
-				if ( rom[ip] % 1000 > 100) {
-					param1 = rom[ip+1];
-				} else {
-					param1 = rom[rom[ip+1]];
-				}
-				if (rom[ip]%10000 > 1000) {
-					param2 = rom[ip+2];
-				} else {
-					param2 = rom[rom[ip+2]];
-				}
-				loc = rom[ip+3];
-				if (param1 == param2) {
-					rom[loc] = 1;
-				} else {
-					rom[loc] = 0;
-				}
-				ip = ip + 4;
-				break;
-			case quit:
-				cout << "exit op" << endl;
-				return;
-			default:
-				cout << "bad op: " << rom[ip] << endl;
-				return;
-		}
-	}
-}
